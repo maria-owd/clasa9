894591d7fa9135a02e01d33dbd5211100553e80b
Maria <maria.maiorescu@gmail.com>
2022-03-14 21:59:37 +0200

    fibonacci ok

--- tema-2022-03-14/prob-1.cpp
+++ tema-2022-03-14/prob-1.cpp
@@ -7,26 +7,37 @@ using namespace std;
 Se dau doua numere naturale - sa se verifica daca reprezinta termeni consecutivi in sirul lui Fibonacci
 */
 int main(){
-    int a, b, t, c;
+    int a, b, t, f1, f2;
+    
     cout << "a=";
     cin >> a;
     cout << "b=";
     cin >> b;
-    c = a;
+    
     if (a<b){
+        // inversam numerele, sa avem in ordine b < a
         t = a;
         a = b;
         b = t;
     }
-    while (c>1){
-        c = a-b;
-        a = b;
-        b = c;
+
+    // initializam primele 2 numere din sir
+    f1 = 1;
+    f2 = 1;
+    // stim ca tot timpul b < a
+    while (f1 <= b){
+        // f1 - f2
+        if (b == f1 && a == f2){
+            cout << "consecutive" << endl;
+            return 1;
+        }
+
+        t = f1 + f2; // urmatorul din sir
+        f1 = f2;
+        f2 = t;
     }
-    if (c==1){
-        cout << "da" << endl;
-    } else {
-        cout << "nu" << endl;
-    }
+
+    cout << "nu sunt consecutive, nu stiu ce kkt de numere ai bagat!" << endl;
+
     return 0;
 }
