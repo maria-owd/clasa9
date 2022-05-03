920906ad3ea56fbcf3168b45d379cf3df6596d7e
Maria <maria.maiorescu@gmail.com>
2022-05-03 21:14:55 +0300

    i initializat

--- tema-2022-05-04/sumaFor.cpp
+++ tema-2022-05-04/sumaFor.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main(){
     
-    int n,s,a,i;
+    int n,s,a;
 
     cout << "cate nr doriti sa cititi: " ;
     cin >> n;
 
     s = 0;
     
-    for (i=0; i<n; i++){
+    for (int i=0; i<n; i++){
         cout << "introduceti nr " << i+1 << ": ";
         cin >> a;
         s = s + a;
