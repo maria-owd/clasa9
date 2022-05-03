707c3f39a9c2c4d07c7730b093a09f2c76df5c94
Maria <maria.maiorescu@gmail.com>
2022-05-03 11:21:29 +0300

    combinatii toate nr unice

--- tema-2002-05-03/toateNr.cpp
+++ tema-2002-05-03/toateNr.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 int main(){
 
-    int n, c, numarulCifrelor, ordinPrimaCifra, copieN, x;
+    int n, c, numarulCifrelor, numarulCifrelor2, ordinPrimaCifra;
+    int copieN, origN, x, x1, copieNrCifre, y, n1, aparitii;
 
     cout << "n=";
     cin >> n;
     copieN = n;
+    origN = n;
 
     // calculam numarul cifrelor si ordinul primei cifre
     numarulCifrelor = 0;
@@ -22,7 +24,7 @@ int main(){
     ordinPrimaCifra = ordinPrimaCifra / 10;
 
     //calculam toate combinatiile de atatea ori cate cifre are numarul
-
+    copieNrCifre = numarulCifrelor;
     n = copieN;
     while (numarulCifrelor > 0){
 
@@ -34,10 +36,34 @@ int main(){
         //mutam prima cifra in spatele numarlui
         x = copieN - c * ordinPrimaCifra; // numarul fara prima cifra
         n = x * 10 + c; // numarul schimbat
-        copieN = n;
         
-        cout << n << endl;
+        // verificam daca ultima cifra este diferita de prima cifra
+        // verificam daca combinatia mai apare, 
+        // adica calculam inca o data toate combinatiile
+        aparitii = 0;
+        numarulCifrelor2 = copieNrCifre;
+        x = copieN;
+        while (numarulCifrelor2 > 0 ){
+            x1 = x;
+            while (x != 0){
+                c = x % 10;
+                x = x / 10;
+            }
+            n1 = (x1 - c*ordinPrimaCifra)*10 + c;
+            if (n1 == n){
+                aparitii++;
+            }
+            x = n1;
+            numarulCifrelor2--;
+        }
 
+        // afisam doar daca nr. nu mai apare si e diferit de cel original
+        if (aparitii==1 && n!=origN){
+            cout << n << endl;
+        }
+        
+
+        copieN = n;
         numarulCifrelor--;
     }
 
